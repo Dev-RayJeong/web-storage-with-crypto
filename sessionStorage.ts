bc54ac62eb05a734002805fe1c996b1b505d7b93
Dev-RayJeong <49974402+Dev-RayJeong@users.noreply.github.com>
2021-06-10 17:58:42 +0900

    Update sessionStorage.ts

--- sessionStorage.ts
+++ sessionStorage.ts
@@ -1,6 +1,6 @@
 import CryptoJS from 'crypto-js';
 
-const KEY = 'gXd4%04@2!5nsd#d0';
+const KEY = 'UNIQUE_KEY_WHAT_YOU_WANT';
 const getEncryptedStr = (str: string, key: string): string => CryptoJS.AES.encrypt(str, key).toString();
 const getDecryptedStr = (str: string, key: string): string =>
   CryptoJS.AES.decrypt(str, key).toString(CryptoJS.enc.Utf8);
